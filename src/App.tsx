dbf448de9552ec0ceaac9c7f80358e46679cac8e
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2025-11-05 21:34:06 +0100

    Added prettier plugin tailwind, animated copy btn, button hover effect, show current color on hover

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,14 @@ import { convertColors } from "./utils/convertColors.ts";
 import Canvas from "./components/Canvas";
 import Palette from "./components/Palette";
 import Toolbar from "./components/Toolbar";
-import Modal from "./components/Modal";
 import { ThemeProvider } from "./contexts/ThemeProvider.tsx";
 import ToggleTheme from "./components/ToggleTheme.tsx";
 
 function App() {
   const [palette, setPalette] = useState([]);
   const [customPalette, setCustomPalette] = useState([]);
-  const [showModal, setShowModal] = useState(false);
   const [typePalette, setTypePalette] = useState(undefined);
   const toolbarRef = useRef(null);
-
-  console.log("customPalette:", customPalette)
-
-  function handleShowModal(e) {
-    let activeModal = e.target.dataset.colors;
-    setShowModal((sm) => (sm = sm === activeModal ? false : activeModal));
-  }
 
   function handleSetTypePalette(e) {
     let numberPalette = e.target.dataset.tab;
@@ -33,30 +24,17 @@ function App() {
 
   return (
     <ThemeProvider>
-      <div className="bg-background min-h-screen w-full flex flex-col gap-12 text-foreground lg:[--spacing-nav:80px] [--spacing-nav:40px] px-2 sm:px-4 md:px-10">
+      <div className="bg-background text-foreground flex min-h-screen w-full flex-col gap-12 px-2 [--spacing-nav:40px] sm:px-4 md:px-10 lg:[--spacing-nav:80px]">
         <ToggleTheme />
 
-        <div className="flex xl:flex-row flex-col lg:gap-16 xl:items-end mt-(--spacing-nav)">
+        <div className="mt-(--spacing-nav) flex flex-col gap-4 sm:gap-8 md:gap-20 xl:flex-row xl:items-end">
           <Canvas setPalette={setPalette} setCustomPalette={setCustomPalette} />
           {palette.length ? (
             <Palette palette={palette} customPalette={customPalette} />
           ) : null}
         </div>
 
-        {palette.length ? (
-          <Toolbar handleShowModal={handleShowModal} toolbarRef={toolbarRef} />
-        ) : null}
-
-        {showModal && (
-          <Modal
-            typePalette={typePalette}
-            setShowModal={setShowModal}
-            showModal={showModal}
-            handleSetTypePalette={handleSetTypePalette}
-            palette={palette}
-            toolbarRef={toolbarRef}
-          />
-        )}
+        {palette.length ? <Toolbar toolbarRef={toolbarRef} /> : null}
       </div>
     </ThemeProvider>
   );
